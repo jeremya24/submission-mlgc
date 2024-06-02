10863d8c4361a8e8acd77d09a6ad9b25f08132bc
Jeremya <jeremiatampubolon025@gmail.com>
2024-06-02 23:19:48 +0700

    Push code to remote repository

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const InputError = require('../exceptions/InputError');
  
 (async () => {
     const server = Hapi.server({
-        port: 5000,
+        port: 8080,
         host: "0.0.0.0",
         routes: {
             cors: {
